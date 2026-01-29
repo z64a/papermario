6bbf601989103ccb1b6e8c1cbaea4bcb408ff185
z64a <cloverhax@gmail.com>
2026-01-29 15:53:06 -0500

    push grids match

--- tools/build/map_features.py
+++ tools/build/map_features.py
@@ -439,22 +439,24 @@ def add_block_grid_defines(lines: list[str], marker: JsonMarker, namespace: str)
     gi = marker.gridComp.gridIndex
     size_x = marker.gridComp.gridSizeX
     size_z = marker.gridComp.gridSizeZ
+    occupants = marker.gridComp.occupants or []
+    useGravity = marker.gridComp.gridUseGravity
 
     lines.append(
         f"#define {namespace}_GRID_PARAMS {gi}, {size_x}, {size_z}, {px}, {py}, {pz}, {NULL_STR}"
     )
 
-    occs = marker.gridComp.occupants or []
-    if len(occs) == 0:
+    if len(occupants) == 0:
         lines.append(f"#define {namespace}_GRID_CONTENT \\")
         lines.append("    Set(LVar0, LVar0) \\") # NOP
         return
 
-    # build grid[sizeX][sizeZ] filled with 0
-    grid = [[0 for _ in range(size_z)] for _ in range(size_x)]
+    lines.append(f"#define {namespace}_GRID_CONTENT \\")
 
-    # fill occupants: grid[x][z] = typeID
-    for occ in occs:
+    if useGravity:
+        lines.append("    Call(SetPushBlockFallEffect, 0, Ref(N(push_block_handle_fall))) \\")
+
+    for occ in occupants:
         if len(occ) < 3:
             raise ValueError(f"BlockGrid marker '{marker.name}' has malformed occupant {occ!r}")
         ox, oz, type_id = occ[0], occ[1], occ[2]
@@ -464,16 +466,11 @@ def add_block_grid_defines(lines: list[str], marker: JsonMarker, namespace: str)
                 f"BlockGrid marker '{marker.name}' occupant out of bounds: x={ox}, z={oz}, "
                 f"gridSizeX={size_x}, gridSizeZ={size_z}"
             )
-        grid[ox][oz] = int(type_id)
 
-    lines.append(f"#define {namespace}_GRID_CONTENT \\")
-    for j in range(size_z):
-        for i in range(size_x):
-            cur = grid[i][j]
-            if cur == 0:
-                lines.append(f"    Call(SetPushBlock, {gi}, {i}, {j}, PUSH_GRID_OBSTRUCTION) \\")
-            else:
-                lines.append(f"    Call(SetPushBlock, {gi}, {i}, {j}, PUSH_GRID_BLOCK) \\")
+        if type_id == 1:
+            lines.append(f"    Call(SetPushBlock, {gi}, {ox}, {oz}, PUSH_GRID_BLOCK) \\")
+        elif type_id == 2:
+            lines.append(f"    Call(SetPushBlock, {gi}, {ox}, {oz}, PUSH_GRID_OBSTRUCTION) \\")
 
 
 def add_block_grid_defines_dx(lines: list[str], marker: JsonMarker, namespace: str) -> None:
